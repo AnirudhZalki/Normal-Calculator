08088cdcfecb60cce53ac32225f16eb8e9aabac0
techA-Z <158753272+AnirudhZalki@users.noreply.github.com>
2024-09-16 19:46:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function a()
+		{
+			let n1=parseFloat(document.getElementById('n1').value)
+			let n2=parseFloat(document.getElementById('n2').value)
+			let operation=document.getElementById('operation').value
+			let result
+			if(operation=="sum")
+			{
+				result=n1+n2
+			}
+			else if(operation=="sub")
+			{
+				result=n1-n2
+			}
+			else if(operation=="multiply")
+			{
+				result=n1*n2
+			}
+			else if(operation=="div")
+			{
+				result=n1/n2
+			}
+
+			else
+			{
+				alert("Error")
+			}
+
+			alert(result)
